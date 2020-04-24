07f4ebab9b084afd0534b32b41283ae59a23ed8d
S10N <sion.jeremiah@gmail.com>
2020-04-24 11:48:10 +0000

    all test pass

--- scripts/fizz.js
+++ scripts/fizz.js
@@ -1,7 +1,7 @@
 
 fizzBuzz = function(number) {
     if (number % 3 === 0 && number % 5 === 0) {
-        return "FizzBuzz";
+        return "Fizzbuzz";
     }
     else if (number % 3 === 0) {
         return "Fizz";
